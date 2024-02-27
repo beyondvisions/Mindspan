0c345947b2324b04e973f765b6f201198be50205
feee08 <111534962+wisemhm@users.noreply.github.com>
2024-02-27 18:07:52 +0100

    Update ContactComponent.jsx

--- client/src/pages/ContactUs/ContactComponent.jsx
+++ client/src/pages/ContactUs/ContactComponent.jsx
@@ -53,42 +53,42 @@ function ContactComponent() {
 
     return (
         <div className={styles.containerPro}>
-            <div className={styles.background}>
-                <div className={styles.container}>
-                    <div className={styles.screen}>
-                        <form className={styles.screen_body} onSubmit={handleSubmit} onReset={handleReset}>
-                            <div className={styles.screen_body_item_left}>
-                                <div className={styles.app_title}>
-                                    <span>We're all</span>
-                                    <span>ears</span>
-                                </div>
-                                <div className={styles.app_contact}><b>Contact information : </b>wissham25@gmail.com </div>
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <div className={styles.screen}>
+                    <form className={styles.screen_body} onSubmit={handleSubmit} onReset={handleReset}>
+                        <div className={styles.screen_body_item_left}>
+                            <div className={styles.app_title}>
+                                <span>We're all</span>
+                                <span>ears</span>
                             </div>
-                            <div className={styles.screen_body_item}>
-                                <div className={styles.app_form}>
-                                    <div className={styles.app_form_group}>
-                                        <input className={styles.app_form_control} placeholder="Nom" name="name" value={formData.name} onChange={handleChange} />
-                                    </div>
-                                    <div className={styles.app_form_group}>
-                                        <input className={styles.app_form_control} placeholder="Email" name="email" value={formData.email} onChange={handleChange} />
-                                    </div>
-                                    <div className={styles.app_form_group}>
-                                        <input className={styles.app_form_control} placeholder="Sujet" name="topic" value={formData.topic} onChange={handleChange} />
-                                    </div>
-                                    <div className={styles.app_form_group_message}>
-                                        <input className={styles.app_form_control} placeholder="Message" name="message" value={formData.message} onChange={handleChange} />
-                                    </div>
-                                    <div className={styles.app_form_group_buttons}>
-                                        <button className={styles.app_form_button} type="reset">Annuler</button>
-                                        <button className={styles.app_form_button} type="submit">Envoyer</button>
-                                    </div>
+                            <div className={styles.app_contact}><b>Contact information : </b>wissham25@gmail.com </div>
+                        </div>
+                        <div className={styles.screen_body_item}>
+                            <div className={styles.app_form}>
+                                <div className={styles.app_form_group}>
+                                    <input className={styles.app_form_control} placeholder="Name" name="name" value={formData.name} onChange={handleChange} />
+                                </div>
+                                <div className={styles.app_form_group}>
+                                    <input className={styles.app_form_control} placeholder="Email" name="email" value={formData.email} onChange={handleChange} />
+                                </div>
+                                <div className={styles.app_form_group}>
+                                    <input className={styles.app_form_control} placeholder="Topic" name="topic" value={formData.topic} onChange={handleChange} />
+                                </div>
+                                <div className={styles.app_form_group_message}>
+                                    <input className={styles.app_form_control} placeholder="Message" name="message" value={formData.message} onChange={handleChange} />
+                                </div>
+                                <div className={styles.app_form_group_buttons}>
+                                    <button className={styles.app_form_button} type="reset">Cancel</button>
+                                    <button className={styles.app_form_button} type="submit">Send</button>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
     );
 }
 
