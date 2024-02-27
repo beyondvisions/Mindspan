eb7e02ba801aaa993dcc750b1f5eeb3f5401f5cd
feee08 <111534962+wisemhm@users.noreply.github.com>
2024-02-27 19:43:38 +0100

    Update RecentPost.jsx

--- client/src/pages/RecentPost.jsx
+++ client/src/pages/RecentPost.jsx
@@ -4,7 +4,7 @@ import PostCard from '../components/PostCard';
 
 export default function RecentPost({ category, subcategory = '' ,}) {
   const [posts, setPosts] = useState([]);
-
+  let selectedLabel = '';
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/post/getPosts?limit=3sort=desc&category=${category}&subcategory=${subcategory}`);
@@ -13,6 +13,15 @@ export default function RecentPost({ category, subcategory = '' ,}) {
     };
     fetchPosts();
   }, [category, subcategory]); 
+  if ( category  === 'événements') {
+    selectedLabel = "/Evenement?searchTerm=&sort=desc&category=événements&subcategory=";
+  } else if ( category === 'santé et mentale') {
+    selectedLabel = "/MentalHelath?searchTerm=&sort=desc&category=événements&subcategory=";
+  } else if ( category === 'parent et enfants') {
+    selectedLabel = "/ParentEtEnfant?searchTerm=&sort=desc&category=événements&subcategory=";
+  } else if ( category === 'Cabinet') {
+    selectedLabel = "/cabinet?adresse=";
+  } 
 
 
   return (
@@ -21,7 +30,7 @@ export default function RecentPost({ category, subcategory = '' ,}) {
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'               style={{color:'#873260'}}
->Les articles de {category === 'Santé et Mentale' ? 'santé mentale' : category} récdntes
+>Les articles de {category === 'Santé et Mentale' ? 'santé mentale' : category} récentes
 </h2>
 <div class="flex flex-col gap-10 md:flex-row md:justify-between md:gap-10">
                {posts.map((post) => (
@@ -29,8 +38,8 @@ export default function RecentPost({ category, subcategory = '' ,}) {
               ))}
             </div>
             <Link
-              to={'/search'}
-              className='text-lg text-white hover:underline text-center'
+ to={selectedLabel}
+               className='text-lg text-white hover:underline text-center'
               style={{color:'#873260'}}
               >
               Voir tous les postes
