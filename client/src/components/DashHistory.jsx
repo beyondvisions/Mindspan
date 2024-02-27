9fec8eeb062e027a4b1ae5293420e53cb346af81
feee08 <111534962+wisemhm@users.noreply.github.com>
2024-02-27 12:41:35 +0100

    Update DashHistory.jsx

--- client/src/components/DashHistory.jsx
+++ client/src/components/DashHistory.jsx
@@ -7,7 +7,7 @@ export default function DashHistory() {
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
-      <h1 className='my-7 text-center font-semibold text-3xl'>Comment History</h1>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Historique des commentaires</h1>
       
       <UserComments/>
     
